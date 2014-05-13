1d22c3318daea55c27350c29975d0a289b2c9920
Sunny Ripert <sunny@sunfox.org>
2014-05-13 23:06:46 +0200

    Initial commit

---
+++ lib/carrierwave/processing/dominant_color.rb
@@ -0,0 +1,9 @@
+require "carrierwave/processing/dominant_color/version"
+
+module Carrierwave
+  module Processing
+    module DominantColor
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/carrierwave/processing/dominant_color/version.rb
@@ -0,0 +1,7 @@
+module Carrierwave
+  module Processing
+    module DominantColor
+      VERSION = "0.0.1"
+    end
+  end
+end
